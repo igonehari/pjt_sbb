aa9bb56d4bae11b618ba873c71431dbb5c98e738
igonehari <ghkdneh@gmail.com>
2026-08-20 12:49:06 +0900

    내이게이션 바 추가

---
+++ src/main/java/org/example/sbb/SampleController.java
@@ -0,0 +1,15 @@
+package org.example.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@RequestMapping("/sample")
+@Controller
+public class SampleController {
+    @GetMapping("/sample/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello Sample";
+    }
+}
